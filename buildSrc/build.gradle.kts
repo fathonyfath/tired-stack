3ed0a7df93a8bab336a9c5cc2f7ac3c72f80556f
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2026-05-19 10:29:11 +0900

    add linter and formatter

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,4 +7,5 @@ dependencies {
     implementation(libs.plugins.kotlin.serialization.map { "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}" })
     implementation(libs.plugins.ktor.map { "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}" })
     implementation(libs.plugins.node.gradle.map { "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}" })
+    implementation(libs.plugins.ktlint.map { "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}" })
 }
